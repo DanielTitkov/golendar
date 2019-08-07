caa95a6c73ccf996669500b14f8e02b627a6eb37
DanielTitkov <mytasmic@gmail.com>
2019-08-07 17:49:42 +0300

    map storage

---
+++ internal/event.go
@@ -0,0 +1,16 @@
+package internal
+
+import (
+	"github.com/google/uuid"
+)
+
+// Event is an event model for go-calendar
+type Event struct {
+	UUID     uuid.UUID
+	Title    string
+	Datetime string
+	Duration string
+	Desc     string
+	User     string
+	Notify   string
+}
